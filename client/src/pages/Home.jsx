fb473e7a5f82aa3acd8ba73c87c9d5b868cb096a
cheeter_Lee <johnson991210@gmail.com>
2022-12-19 22:29:53 +0800

    finished frontend

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
+// components
+import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
+
+
+const Home = () => {
+    const { workouts, dispatch } = useWorkoutsContext()
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const data = await response.json()
+
+            if (response.ok) {
+                dispatch({
+                    type: 'SET_WORKOUTS',
+                    payload: data
+                })
+            }
+        }
+        
+        fetchWorkouts()
+        // eslint-disable-next-line
+    }, []) 
+
+    return (
+        <div className='home'>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
+            <WorkoutForm />
+        </div>
+    )
+}
+
+export default Home
